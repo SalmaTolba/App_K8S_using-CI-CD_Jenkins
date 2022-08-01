2a3c363ad5416f92af954f946d0c1b2ac8279ca4
salma <salmaehabtolba1998@gmail.com>
2022-08-01 06:18:22 +0200

    update infrastructure commit

--- network/nat.tf
+++ network/nat.tf
@@ -1,36 +1,30 @@
 resource "google_compute_router" "router" {
-  name    = "router"
+  name = "router"
+  network = google_compute_network.vpc_network.name
+}
+
+resource "google_compute_address" "ipnat" {
+  name = "ipnat"
+  project = var.project_id
   region  = var.region
-  network = google_compute_network.vpc_network.id
 }
 
-resource "google_compute_address" "ip_nat" {
-  name         = "ip-nat"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
-
-}
 
 resource "google_compute_router_nat" "nat" {
-  name   = "nat"
-  router = google_compute_router.router.name
-  region = var.region
-
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  name                               = "nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "MANUAL_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ips = [ google_compute_address.ipnat.self_link ]
 
   subnetwork {
     name                    = google_compute_subnetwork.Management_Subnet.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    source_ip_ranges_to_nat = [var.Management_subnet_cidr]
   }
 
-
-  subnetwork {
-    name                    = google_compute_subnetwork.Restricted_Subnet.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  subnetwork { 
+     name = google_compute_subnetwork.Restricted_Subnet.id
+     source_ip_ranges_to_nat = [ var.Restricted_subnet_cidr] # "ALL_IP_RANGES"
   }
-
-  #depends_on = [google_compute_address.ip_nat.name]
-  nat_ips = [google_compute_address.ip_nat.self_link]
 }
-
